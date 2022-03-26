f14adb21e80c09f735060bc66c971c8b32676948
4yuub <karafi.work@gmail.com>
2022-03-26 15:48:08 +0100

    chore: solve ex02 in cpp module07

---
+++ module07/ex02/Array.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+template <typename T>
+class Array {
+public:
+    Array();
+    Array(unsigned int size);
+    Array(const Array& other);
+    ~Array();
+    Array& operator=(const Array& other);
+    T& operator[](unsigned int index);
+    const T& operator[](unsigned int index) const;
+    unsigned int size() const;
+private:
+    T* m_array;
+    unsigned int m_size;
+};
+
+#include "Array.tpp"
