06597b049c9da61cbc97c19dfb7d48bd3a66d670
4yuub <karafi.work@gmail.com>
2022-03-28 23:56:38 +0000

    fix: errors where reviewing guidelines

--- module07/ex01/iter.hpp
+++ module07/ex01/iter.hpp
@@ -3,7 +3,7 @@
 #include <string>
 
 template<typename T>
-void iter(T *array, size_t size, void(*f)(T&)) {
+void iter(T *array, size_t size, void(*f)(T const &)) {
     for (size_t i = 0; i < size; i++) {
         f(array[i]);
     }
--- module07/ex01/main.cpp
+++ module07/ex01/main.cpp
@@ -1,24 +1,20 @@
 #include "iter.hpp"
 
-void times_two(int &n) {
-    n *= 2;
+void times_two(int const &n) {
+    std::cout << n * 2 << std::endl;
 }
 
-void add_ft_prefix(std::string &s) {
-    s = "ft_" + s;
+void print(std::string const &s) {
+    std::cout << s << std::endl;
 }
 
 int main(void) {
     int array[] = {1, 2, 3, 4, 5};
 
     ::iter(array, 5, times_two);
-    for (int i = 0; i < 5; i++)
-        std::cout << array[i] << std::endl;
 
     std::string strings[] = {"s1", "s2", "s3"};
 
-    ::iter(strings, 3, add_ft_prefix);
-    for (int i = 0; i < 3; i++)
-        std::cout << strings[i] << std::endl;
+    ::iter(strings, 3, print);
     
 }
